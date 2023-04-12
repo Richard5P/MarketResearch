7f7d5356d57464a93df83d4fded9074a391dbf33
Ric Deutsch <richard.deutsch@gmail.com>
2023-04-12 17:52:54 +0100

    boilerplate: Add app description

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+The app is a Market Research sample tool to demonstrate how Python can be used
+to prepare and present a typical market reseach report for business analysis.
+There are two primary services: 
+    1) Import statistical demographical data from an external file
+    2) Prepare and present an ad-hoc market analysis report
+The functions for each of those 2 services are contained in separate .py files.
+    1)import.py
+    2)report.py
+This run.py contains the functions to:
+   - initiate the app
+   - select the service to run
+   - call the selected service
+   - return a validation of the service run
+   - allow the user to select another service or exit the app
+"""
