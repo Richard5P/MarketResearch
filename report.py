05816896f493729d467b9e52c28ebfcb4e4d7777
Richard5P <richard.deutsch@gmail.com>
2023-04-22 11:43:14 +0000

    Add introductory docstrings to report.py file

--- report.py
+++ report.py
@@ -1,4 +1,11 @@
-### start of input classes and functions
+"""
+This file contains the classes, function and variables for
+the production of statistical reports. The file is meant to be
+imported into a main file for deployment.
+The features included in this file include:
+    - User processes for configuring, running and outputting each report
+"""
+
 class InvalidPercents(Exception):
     """
     Raise when percents don't add up to 100
