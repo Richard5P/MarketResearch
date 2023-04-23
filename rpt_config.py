3eaef50e9e76f5c7fa153a307509d9b129b46ab0
Richard5P <richard.deutsch@gmail.com>
2023-04-23 10:58:16 +0000

    feat: Add input_years to rpt_config

--- rpt_config.py
+++ rpt_config.py
@@ -1,3 +1,5 @@
+import datetime
+
 """
 This file contains the classes, function and variables for
 the user processes for configuring the report calculations. 
@@ -9,7 +11,16 @@ class InvalidPercents(Exception):
     """
     pass
 
+class InvalidDateRange(Exception):
+    """
+    Raise when years are out of range
+    """
+    pass
+
 def input_weights():
+    """
+    Prompts user for study weight report configuration
+    """
     print(f'There are 3 report studies available for your report:')
     print(f'\t Disposable Income, Population, Urbanisation\n')
     print(f'Please enter 3 numbers which total to 100 for weighting the percent of each attribute\n')
@@ -30,23 +41,30 @@ def input_weights():
         except ValueError:
             print('\nNumbers only, please try again')
 
-def input_years():
-    print(f'The range of years studies available for your report are ')
-    print(f'\t Disposable Income, Population, Urbanisation\n')
-    print(f'Please enter 3 numbers which total to 100 for weighting the percent of each attribute\n')
+def years_loaded(stat_dict):
+    """
+
+    """
+
+def input_years(stat_dict):
+    """
+    Prompts user for date range report configuration
+    """
+    print(f'The range of years studies available for your report are :')
+    first_last_years = years_loaded(stat_dict)
+    print(f'Please enter a start year and an end year within that range (inclusive)\n')
     years_unset = True
     while years_unset:
         try:
-            disp_pct = int(input('Disposable Income %:\n'))
-            popu_pct = int(input('Population %:\n'))
-            urba_pct = int(input('Urbanisation %:\n'))
-            if (disp_pct + popu_pct + urba_pct != 100):
-                raise InvalidPercents
+            start_year = int(input('Report start year:\n'))
+            end_year = int(input('Report end year %:\n'))
+            if !(start_year >= first_last_years[0] and end_year <= first_last_years[1]):
+                raise InvalidDateRange
             else:
-                return([disp_pct,popu_pct,urba_pct])
+                return([start_year, end_year])
         
-        except InvalidPercents:
-            print('\nAmounts entered do not sum to 100, please try again')
+        except InvalidDataRange:
+            print(f'\nYears must be between {range_list[0]} and {range_list[1]}, please try again')
 
 def input_rpt_options(weights, years, regions, stat_dict):
     """
@@ -55,7 +73,5 @@ def input_rpt_options(weights, years, regions, stat_dict):
     """
     print('Next step is to configure your report\n')
     weights = input_weights()
-    print(weights)
-    return(weights)
-#    years = input_years()
+    years = input_years(stat_dict)
 #    regions = input_regions()
